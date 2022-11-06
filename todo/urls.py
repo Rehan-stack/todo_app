81d014ed6712443f9a41933e64cf56f3e1144bbe
Rehan-stack <rehanahmedahmed007@gmail.com>
2022-11-06 22:05:29 +0500

    init

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+
+from todo.views import*
+
+
+urlpatterns = [
+    path('', todo),
+    
+]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def  todo(requests):
+    return render(requests, 'todolist.html')
